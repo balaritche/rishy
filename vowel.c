14a6155e5ba33506a812724ee9b924a6412c40ff
balaritche <35653119+balaritche@users.noreply.github.com>
2018-01-25 16:15:29 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char z;
+clrscr();
+printf("enter the char");
+scanf("%c",&a);
+if(z=='a' || z=='e' || z=='i' z=='o' || z=='u')
+{
+printf("vowel");
+}
+else
+{
+printf("constant");
+getch();
+}
+}
