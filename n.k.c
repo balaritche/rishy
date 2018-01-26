c5db4a7a34ae90b738184b8ba055ffdccb14e008
balaritche <35653119+balaritche@users.noreply.github.com>
2018-01-26 11:14:13 +0000

    Create n.k.c

---
+++ n.k.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n,k,c=0,a[i];
+printf("enter the no");
+scanf("%d%d",&n,&k);
+for(i=1,i<=n,i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=1,i<=k,i++)
+{
+c=c+a[i]:
+}
+printf("%d",c);
+}
