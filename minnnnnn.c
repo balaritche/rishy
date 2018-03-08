291edf0d911ad6a6c733f2e05441efa9de089fc5
balaritche <35653119+balaritche@users.noreply.github.com>
2018-03-08 16:20:56 +0530

    Create minnnnnn.c

---
+++ minnnnnn.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i;
+    printf("Enter the numbers:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    printf("%d",i);
+if(i=1)
+printf("\nmin no: %d",i);
+}
+
+
