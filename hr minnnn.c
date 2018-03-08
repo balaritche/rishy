b5ea8a57d38b053897ecf0acdac6a114eb7e09ae
balaritche <35653119+balaritche@users.noreply.github.com>
2018-03-08 15:54:02 +0530

    Create hr minnnn.c

---
+++ hr minnnn.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int m,hr,rem;
+    printf("Enter the minutes:");
+    scanf("%d",&m);
+hr=m/60;
+rem=m%60;
+printf("hr is:%d \nmin is :%d",hr,rem);
+}
+
+
