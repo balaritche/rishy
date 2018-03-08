b9ed7695a3dcc7600ed2bec3d09599efcbe3e1e6
balaritche <35653119+balaritche@users.noreply.github.com>
2018-03-08 16:09:20 +0530

    Create maxnoooooo.c

---
+++ maxnoooooo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i;
+    printf("Enter the numbers:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    printf("%d",i);
+if(n)
+printf("\nmax no: %d",n);
+}
